c23e6cbddb69f4e54408a5e942f2dc0547076986
lucascoutoandrade <lucascouto.ads@gmail.com>
2026-05-16 18:15:38 -0300

    add: login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,22 @@
+/// <reference types="cypress" />
+
 describe('My First Test', () => {
   it('Does not do much!', () => {
     expect(true).to.equal(true)
   })
 
-it('Navigate to ServeRest',()=> {
-  
-    cy.visit('https://front.serverest.dev/admin/home')
-    
+  it('Login ServeRest', () => {
 
-})
+    cy.visit('https://front.serverest.dev/admin/home')
+
+    cy.get(`input[name='email']`).type(`fulano@qa.com`);
+
+    cy.get(`input[name='password']`).type(`teste`);
+
+    cy.get(`button[type='submit']`).click();
+
+    cy.get(`button[data-testid='logout']`).should(`be.visible`);
+
+  })
 
 })
