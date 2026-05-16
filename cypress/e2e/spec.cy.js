c002928c2eda3d8f4cb01a16dd2d53972058a4b9
lucascoutoandrade <lucascouto.ads@gmail.com>
2026-05-16 19:27:15 -0300

    add: cadastrar usuario feat

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,10 @@
 /// <reference types="cypress" />
 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
+describe('Challenge Serverest ', () => {
 
-  it('Login ServeRest', () => {
+  const nomeAleatorio = gerarNomeAleatorio(10);
+
+  beforeEach(() => {
 
     cy.visit('https://front.serverest.dev/admin/home')
 
@@ -17,6 +16,51 @@ describe('My First Test', () => {
 
     cy.get(`button[data-testid='logout']`).should(`be.visible`);
 
+  });
+
+  it(`Cadastrar Usuario`, () => {
+
+    cy.contains(`Cadastrar Usuários`).click();
+
+    cy.get(`h1`).should('contain', `Cadastro de usuários`);
+
+    cy.get(`input[name='nome']`).type(nomeAleatorio);
+
+    cy.get(`input[name='email']`).type(nomeAleatorio + "@test.com");
+
+    cy.get(`input[name='password']`).type(`teste`);
+
+    cy.get(`button[type='submit'`).click();
+
+    cy.get(`h1`).should('contain', 'Lista dos usuários');
+
+    cy.get('table').contains(nomeAleatorio).should('exist');
+
+
+  });
+
+
+  it(`Listar Usuario`, () => {
+
+
   })
 
-})
+  it(`Cadastrar Produtos`, () => {
+
+
+
+  });
+
+  it(`Listar Produtos`, () => {
+
+
+
+  })
+
+  function gerarNomeAleatorio(tamanho) {
+
+    return Math.random().toString(36).substring(2, 2 + tamanho);
+
+  }
+
+});
