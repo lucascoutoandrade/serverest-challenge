05ba438bb9587a112cb1087734daa6d4c14dfce2
lucascoutoandrade <lucascouto.ads@gmail.com>
2026-05-16 17:31:33 -0300

    add: initial structure

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+
+it('Navigate to ServeRest',()=> {
+  
+    cy.visit('https://front.serverest.dev/admin/home')
+    
+
+})
+
+})
